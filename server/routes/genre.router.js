be34955411a722ce101d9c6e1eafcbcf72deeb10
Anna Sutheim <asutheim@gmail.com>
2023-06-17 10:07:46 -0500

    Set up genre router with query text

--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -1,10 +1,25 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const pool = require('../modules/pool')
+const pool = require("../modules/pool");
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   // Add query to get all genres
-  res.sendStatus(500)
+  const movieToGet = req.body.id;
+  const queryText = `
+ SELECT "genres"."name" FROM genres
+  JOIN movies_genres on genres.id = movies_genres.genre_id
+  JOIN movies on movies.id  = movies_genres.movie_id
+  WHERE movies.id = ${movieToGet}`;
+
+  pool
+    .query(queryText)
+    .then((result) => {
+      res.sendStatus(201);
+    })
+    .catch((error) => {
+      console.log("error getting genres from database", error);
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;
---
+++ src/components/MovieDetail/MovieDetail.jsx
@@ -0,0 +1,2 @@
+//To do:
+//Get request 
