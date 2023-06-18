53c4b9a232b77268aaca8b5e8b6fee8e99ce4f7e
Anna Sutheim <asutheim@gmail.com>
2023-06-17 23:24:49 -0500

    Dropdown menu added to AddMovie component form

--- src/components/AddMovie/AddMovie.jsx
+++ src/components/AddMovie/AddMovie.jsx
@@ -22,10 +22,26 @@ const AddMovie = () => {
       <h3>Add a movie to the list:</h3>
       <form onSubmit={handleSubmit} onChange={handleInput}>
         <input type="text" value="title" placeholder="Title" required />
-        <input type="text" value="image" placeholder="Image URL" />
+        <input type="text" value="image" placeholder="Image URL" /> <br />
         <textarea name="description" id="description" rows="4" cols="50">
           A description of the movie
         </textarea>
+        <select name="genres" id="genres">
+          <option value="Adventure">Adventure</option>
+          <option value="Animated">Animated</option>
+          <option value="Biographical">Biographical</option>
+          <option value="Comedy">Comedy</option>
+          <option value="Disaster">Disaster</option>
+          <option value="Drama">Drama</option>
+          <option value="Epic">Epic</option>
+          <option value="Fantasy">Fantasy</option>
+          <option value="Musical">Musical</option>
+          <option value="Romantic">Romantic</option>
+          <option value="Science Fiction">Science Fiction</option>
+          <option value="Space-Opera">Space-Opera</option>
+          <option value="Superhero">Superhero</option>
+        </select>
+        <input type="submit" value="Submit" />
       </form>
     </div>
   );
